e7b1c7e14bcca43061a551a9edff0fa2f4098ef6
saumilshah66 <shah.saumil66@gmail.com>
2020-09-23 14:05:28 -0400

    Using glob to get all the image names

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <pcl/io/ply_io.h>
+#include <string>
+#include <math.h>
+#include <glob.h>
+#include <vector>
+
+std::vector<std::string> giveFiles(std::string pattern) {
+	glob_t glob_result;
+    glob(pattern.c_str(),GLOB_TILDE,NULL,&glob_result);
+    std::vector<std::string> files;
+    for(unsigned int i=0; i<glob_result.gl_pathc; ++i){
+        files.push_back(std::string(glob_result.gl_pathv[i]));
+    }
+    globfree(&glob_result);
+	return files;
+}
 
 int main(){
-	std::cout<<"Hello World";
+	std::string depthImgPath="/media/saumil/Extra_Linux/Dataset/Dataset/MultipleObjects/scene_035/frames/*depth.png";
+	std::string rgbImgPath="/media/saumil/Extra_Linux/Dataset/Dataset/MultipleObjects/scene_035/frames/*rgb.png";
+	auto depthImages = giveFiles(depthImgPath);
+	auto rgbImages = giveFiles(rgbImgPath);
+	// for(auto name: files) std::cout << name << std::endl;
+	std::cout << depthImages.size() << std::endl;
+	std::cout << rgbImages.size() << std::endl;
 	return 0;
 }
