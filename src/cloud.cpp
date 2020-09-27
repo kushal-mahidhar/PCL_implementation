bcfee5170e88bed52e8aa3a1b61fa45a477d3170
saumilshah66 <shah.saumil66@gmail.com>
2020-09-27 17:25:11 -0400

    Make and visualize cloud

---
+++ src/cloud.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <string>
+#include <math.h>
+#include <vector>
+
+
+void createCloudFromImage(cv::Mat image) {
+	
+}
